02d0326b53362fc181b631600739464f1b35cc66
Ayaan Ahmad <ayanahmad175@gmail.com>
2022-10-07 19:14:28 -0700

    does not wokr

--- A66.hpp
+++ A66.hpp
@@ -5,3 +5,21 @@
 using namespace std;
 
 // complete the function maxmin( )
+
+void maxmin(int n1, int n2, int n3, int & min, int & max) {
+  if ((n1 >= n2) && (n1 >= n3)) {
+    max = n1;
+  } else if ((n2 >= n1) && (n2 >= n3)) {
+    max = n2;
+  } else {
+    max = n3;
+  }
+
+  if ((n1 <= n2) && (n1 <= n3)) {
+    min = n1;
+  } else if ((n2 <= n1) && (n2 <= n3)) {
+    min = n2;
+  } else {
+    min = n3;
+  }
+}
